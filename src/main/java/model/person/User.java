b07b9fcddb9b39281ad7213e9917170232fec4fa
Harnet69 <harnet69@gmail.com>
2020-04-18 20:33:11 +0200

    User can check if the media item is free for rent

--- src/main/java/model/person/User.java
+++ src/main/java/model/person/User.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class User extends Person {
     private static int idCounter = 0;
+    private Mediatheque library = Mediatheque.getInstance();
     private List<MediaItem> rentedItems = new ArrayList<>();
 
     public User(String name, String login, boolean isManager) {
@@ -25,17 +26,18 @@ public class User extends Person {
     }
 
     public void rent(int id){
-        MediaItem rentedItem = Mediatheque.getInstance().getItems().getItemById(id);
+        MediaItem rentedItem = library.getItems().getItemById(id);
         rentedItems.add(rentedItem);
         rentedItem.setRented(true);
     }
 
     public void returnMedia(int id){
-        for(MediaItem item : rentedItems){
-            if(item.getId() == id){
-                rentedItems.remove(item);
-                Mediatheque.getInstance().getItems().getItemById(id).setRented(false);
+        if(!isRented(id)){
+                library.getItems().getItemById(id).setRented(false);
             }
-        }
+    }
+
+    public boolean isRented(int id){
+        return library.getItems().getItemById(id).isRented();
     }
 }
