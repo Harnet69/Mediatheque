d68b3095207d8587544b9bb19c7952f4d6c5a0b4
Harnet69 <harnet69@gmail.com>
2020-04-23 10:12:47 +0200

    Configured web modules

--- src/main/java/config/Initializer.java
+++ src/main/java/config/Initializer.java
@@ -1,5 +1,97 @@
-package config;
+//package com.codecool.shop.config;
 
-public class Initializer {
+//import com.codecool.shop.connect.JDBC;
+//import com.codecool.shop.dao.ProductCategoryDao;
+//import com.codecool.shop.dao.ProductDao;
+//import com.codecool.shop.dao.SupplierDao;
+//import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+//import com.codecool.shop.dao.implementation.ProductDaoMem;
+//import com.codecool.shop.dao.implementation.SupplierDaoMem;
+//import com.codecool.shop.model.Product;
+//import com.codecool.shop.model.ProductCategory;
+//import com.codecool.shop.model.Supplier;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+
+@WebListener
+public class Initializer implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+//        ProductDao productDataStore = ProductDaoMem.getInstance();
+//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+//        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+//
+//
+//        //setting up a new supplier
+//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+//        supplierDataStore.add(amazon);
+//        Supplier lenovo = new Supplier("Lenovo", "Computers");
+//        supplierDataStore.add(lenovo);
+//        Supplier apple = new Supplier("Apple", "Smartphone and tablet producer");
+//        supplierDataStore.add(apple);
+//
+//        //setting up a new product category
+//        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+//        ProductCategory phone = new ProductCategory("Phone", "Hardware", "Smartphones are a class of mobile phones and of multi-purpose mobile computing devices.");
+//        productCategoryDataStore.add(tablet);
+//        productCategoryDataStore.add(phone);
+//
+//        //setting up products
+//        productDataStore.add(new Product("Amazon Fire", 49, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+//        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+//        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+//        productDataStore.add(new Product("iPhone XR", 749, "USD", "The phone has a 6.1-inch 'Liquid Retina' LCD display, which Apple claims is the most advanced LCD in the industry.", phone, apple));
+//        productDataStore.add(new Product("Apple 10.5' iPad Air", 499, "USD", "Updated from the same one you know and love, features a few improvements to provide you with a better experience", tablet, apple));
+//
+//        // setting products to db from sql file (if you want to reset db to default - move init_db.sql file to sql folder)
+//        // test to see if a file exists
+//        File f = new File("src/main/sql/", "init_db.sql");
+//        if(f.exists()) {
+//            System.out.println("Exists!");
+//            try {
+//                executeUpdateFromFile("src/main/sql/init_db.sql");
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }else{
+//            System.out.println("File-backup init_db.sql in resources folder!");
+//        }
+    }
+
+    public void executeUpdateFromFile(String filePath) {
+            String query = "";
+            try {
+                query = new String(Files.readAllBytes(Paths.get(filePath)));
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executeUpdate(String query) throws SQLException {
+//        try (Connection connection = JDBC.openConnection()) {
+//
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            statement.executeUpdate();
+//
+//        } catch (SQLTimeoutException e) {
+//            System.err.println("ERROR: SQL Timeout");
+//        }
+    }
 }
--- src/main/java/controller/MediathequeController.java
+++ src/main/java/controller/MediathequeController.java
@@ -1,5 +1,24 @@
 package controller;
 
-public class MediathequeController {
-    
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/"})
+public class MediathequeController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        TemplateEngine engine = com.codecool.shop.config.TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+//            context.setVariable("products", productDataStore.getAll());
+        engine.process("index.html", context, resp.getWriter());
+    }
 }
